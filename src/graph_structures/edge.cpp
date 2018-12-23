f38ed5c0b10121b5b1fd516a306b84acc6a7bf2b
Dunja Vesinger <dunja.vesinger@gmail.com>
2018-12-23 16:44:52 +0100

    Edge implementation

--- src/graph_structures/edge.cpp
+++ src/graph_structures/edge.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "edge.h"
+
+Edge::Edge(PafEntry entry){
+    id_end_ = entry.GetTargetId();
+    overlap_score_ = entry.GetOverlapScore();
+    extension_score_ = entry.GetExtensionScore();
+    sequence_identity_ = entry.GetSequenceIdentity();
+    overlap_length_  = entry.GetOverlapLength();
+
+    overhang_start_ = entry.getQueryStart();
+    overhang_end_ = entry.getQueryEnd();
+    extension_len_start_ = entry.GetExtensionLenStart();
+    extension_len_end_= entry.GetExtensionLenEnd();
+    overlap_len_start_ = entry.GetOverlapLenStart();
+    overlap_len_end_ = entry.GetOverlapLenEnd();
+}
+
+std::string Edge::GetIdEnd(){
+    return id_end_;
+}
+
+double Edge::GetOverlapScore(){
+    return overlap_score_;
+}
+
+double Edge::GetExtensionScore(){
+    return extension_score_;
+}
+
+double Edge::GetSequenceIdentity(){
+    return sequence_identity_;
+}
+
+long Edge::GetOverlapLength(){
+    return overlap_length_;
+}
+
+long Edge::GetOverhangStart(){
+    return overhang_start_;
+}
+
+long Edge::GetOverhangEnd(){
+    return overhang_end_;
+}
+
+long Edge::GetExtensonLenStart(){
+    return extension_len_start_;
+}
+
+long Edge::GetExtensonLenEnd(){
+    return extension_len_end_;
+}
+
+long Edge::GetOverlapLenStart(){
+    return overlap_len_start_;
+}
+
+long Edge::GetOverlapLenEnd(){
+    return overlap_len_end_;
+}
--- src/graph_structures/edge.h
+++ src/graph_structures/edge.h
@@ -3,6 +3,7 @@
 //
 
 #include <string>
+#include "paf_entry.h"
 
 #ifndef SCAFFOLD_EDGE_H
 #define SCAFFOLD_EDGE_H
@@ -16,12 +17,29 @@ private:
     double sequence_identity_;
     long overlap_length_;
 
-    long oh1_;
-    long oh2_;
-    long el1_;
-    long el2_;
-    long ol1_;
-    long ol2_;
+    long overhang_start_; //oh1
+    long overhang_end_; //oh2
+    long extension_len_start_; //el1
+    long extension_len_end_; //el2
+    long overlap_len_start_; //ol1
+    long overlap_len_end_; //ol2
+
+public:
+    //constructor
+    Edge(PafEntry entry);
+
+    //getters
+    std::string GetIdEnd();
+    double GetOverlapScore();
+    double GetExtensionScore();
+    double GetSequenceIdentity();
+    long GetOverlapLength();
+    long GetOverhangStart();
+    long GetOverhangEnd();
+    long GetExtensonLenStart();
+    long GetExtensonLenEnd();
+    long GetOverlapLenStart();
+    long GetOverlapLenEnd();
 };
 
 
