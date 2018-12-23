303bbb099fcdbd50e2ef7066488097d925d411a1
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2018-12-23 19:40:47 +0100

    file utils doc to header

--- src/utils/file_utils.h
+++ src/utils/file_utils.h
@@ -11,15 +11,33 @@
 #ifndef SCAFFOLD_FILE_UTILS_H
 #define SCAFFOLD_FILE_UTILS_H
 
-
 namespace file_utils {
 
-    std::unordered_map<std::string, FastaEntry> loadFromFasta(std::string &filename, bool is_conting_file) ;
+/**
+ * Function loads FASTA file to unordered map that maps fasta enrty id to fasta entry object.
+ *
+ * @param filename fasta file name
+ * @param is_conting_file true if given file contains contings, false otherwise
+ *
+ * @returns unordered map that maps fasta entry id to fasta entry object
+ */
+std::unordered_map<std::string, FastaEntry> loadFromFasta(std::string &filename, bool is_conting_file);
 
-    std::vector<PafEntry> loadFromPAF(std::string &file_name);
+/**
+ * Function loads PAF file to vector of paf entries, it also filters PAF entries using filterPafEntries function.
+ *
+ * @param file_name paf file name
+ *
+ * @returns vector of filtered paf entries
+ */
+std::vector<PafEntry> loadFromPAF(std::string &file_name);
 
-    bool filterPafEntries(PafEntry entry);
+/**
+* Function decides should you filter given paf entry.
+* @param entry paf entry for which to decide if it should be filtered
+* @return true if the entry should be filtered
+*/
+bool filterPafEntries(PafEntry entry);
 };
-
 
 #endif //SCAFFOLD_FILE_UTILS_H
